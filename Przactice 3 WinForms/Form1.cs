7407cab85c3c6851c0e521f0084d172b6e1b76fa
antoska53 <32413836+antoska53@users.noreply.github.com>
2017-10-10 00:50:24 +0300

    Кривые
    Сделаны кривые и кнопка очистить

--- Przactice 3 WinForms/Form1.cs
+++ Przactice 3 WinForms/Form1.cs
@@ -12,9 +12,11 @@ namespace Przactice_3_WinForms
 {
     public partial class Form1 : Form
     {
-        List<Point> points = new List<Point>(); // массив точек
-      
+        List<Point> listPoints = new List<Point>(); // массив точек
+
         bool bButtonOn = false; // флаг кнопки 1
+        enum eLineType { None, Curved, Polygone, Beizers, Filled }
+        eLineType LineType = eLineType.None;
         public Form1()
         {
             InitializeComponent();
@@ -30,11 +32,12 @@ namespace Przactice_3_WinForms
             {
                 if (e.Button == MouseButtons.Left)
                 {
-                    Point p = e.Location; // сохраняем координаты курсора и мышки
-                    points.Add(p); // добавляем координаты в массив точек
+                    Point p = new Point(e.Location.X, e.Location.Y); // сохраняем координаты курсора и мышки
+                    listPoints.Add(p); // добавляем координаты в массив точек
                     Refresh(); //перерисовываем
                 }
             }
+
         }
 
        
@@ -43,17 +46,61 @@ namespace Przactice_3_WinForms
            
                 var d = e.Graphics; 
                 Brush br = Brushes.Black; //кисть
-               
-                foreach (Point pnt in points)
-                {
-                    d.FillEllipse(br, pnt.X, pnt.Y, 3, 3); //рисуем точку
-                }
+            Pen pen1 = new Pen(Color.Aqua, width:2);
+           
             
+
+            if (listPoints.Count() > 0)
+            {
+                Point[] arrPoint = new Point[listPoints.Count()];//напрямую через List, почемуто не рисует
+                int i = 0;
+                foreach (Point pnt in listPoints)
+                {
+                    d.FillEllipse(br, pnt.X - 2, pnt.Y - 2, 4, 4); //рисуем точки
+                    arrPoint[i] = pnt;//перезапись List в массив
+                    i++;
+                }
+               
+                if(LineType == eLineType.Curved) {
+                   //d.DrawClosedCurve(pen1, listPoints);
+                    d.DrawClosedCurve(pen1, arrPoint);
+                    LineType = eLineType.None;// отключаем кривую
+                    bButtonOn = false; //отключаем точки
+                   
+                }
+                if(LineType == eLineType.Polygone)
+                {
+                    //d.DrawPolygon(pen1, listPoint);
+                    d.DrawPolygon(pen1, arrPoint);
+                    LineType = eLineType.None;
+                    bButtonOn = false; //отключаем точки
+                }
+                if(LineType == eLineType.Beizers )
+                {
+                    //d.DrawBeziers(pen1, listPoints);
+                    d.DrawBeziers(pen1, arrPoint);
+                    LineType = eLineType.None;
+                    bButtonOn = false; //отключаем точки
+                }
+             
+                if(LineType == eLineType.Filled)
+                {
+                    d.FillClosedCurve(br, arrPoint);
+                    LineType = eLineType.None;
+                    bButtonOn = false; //отключаем точки
+                }
+               
+            }
+           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            if (!bButtonOn) {
+                listPoints.Clear();
+                Refresh();
+            }
             bButtonOn = !bButtonOn;
            
         }
@@ -63,6 +110,43 @@ namespace Przactice_3_WinForms
 
         }
 
-     
+        private void button3_Click(object sender, EventArgs e) //кривая
+        {
+            LineType = eLineType.Curved;
+            Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)//ломаная
+        {
+            LineType = eLineType.Polygone;
+            Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)//Безье
+        {
+            if (listPoints.Count == 4)//рисуем безье если есть 4 точки
+            {
+                LineType = eLineType.Beizers;
+                Refresh();
+            }
+            else bButtonOn = false;//отключаем точки
+        }
+
+        private void button6_Click(object sender, EventArgs e)//заполненная
+        {
+            LineType = eLineType.Filled;
+            Refresh();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)//очистить
+        {
+            listPoints.Clear();
+            Refresh();
+        }
     }
 }
